cfd8b6bf2ad181ecf4b21a0bbc075127b1cb0d84
Nour El Sobky <Nour.ElSobky@tpay.me>
2019-10-03 02:58:38 +0200

    `Add UserDetails class` and implement getUserId

--- src/test/java/TestSteps/UserDetails.java
+++ src/test/java/TestSteps/UserDetails.java
@@ -12,10 +12,11 @@ import static com.jayway.restassured.RestAssured.given;
 public class UserDetails {
     private Response _response = null;
     private JsonPath _jsonPath = null;
+    private String _userId = null;
 
 
     @Test
-    public void GetAllEmails() {
+    public void GetUserId() {
 
         RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
         RestAssured.basePath = "users";
@@ -23,7 +24,17 @@ public class UserDetails {
         String path = RestAssured.baseURI + RestAssured.basePath+"?username=Samantha";
         _response = get(path);
         _jsonPath = new JsonPath(_response.asString());
-        System.out.println(_jsonPath.get().toString());
-        System.out.println(_jsonPath.get("address").toString());
+        _userId = _jsonPath.get("id").toString();
+    }
+
+    @Test
+    public void GetAllRelatedPosts(){
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
+        RestAssured.basePath = "posts";
+        given().contentType(ContentType.JSON);
+        String path = RestAssured.baseURI + RestAssured.basePath+"?userId="+_userId;
+        _response = get(path);
+        _jsonPath = new JsonPath(_response.asString());
+        _userId = _jsonPath.get("id").toString();
     }
 }
