71a26afdf9bed810eea872da7006ca0458d48f0c
Nour El Sobky <Nour.ElSobky@tpay.me>
2019-10-07 01:06:02 +0200

    - Resolve CI issue

---
+++ src/test/java/TestSteps/TestCaseBase.java
@@ -0,0 +1,34 @@
+package TestSteps;
+
+import com.jayway.restassured.http.ContentType;
+import org.json.JSONArray;
+import org.junit.After;
+import org.junit.Before;
+import utils.Utilities;
+
+public class TestCaseBase {
+
+    public String _baseUri;
+    public JSONArray _userDetails;
+    public HelperTestSteps _helper;
+    public String _userId;
+    public JSONArray _userPosts;
+    public JSONArray _comments;
+
+    public TestCaseBase(String baseUri){
+        this._baseUri = baseUri;
+        this._helper = new HelperTestSteps();
+    }
+
+    @Before
+    public void configure() {
+        Utilities.setBaseUri(this._baseUri);
+        Utilities.setAPIContentType(ContentType.JSON);
+    }
+
+    @After
+    public void afterTest(){
+        //Utilities.resetBasePath();
+    }
+
+}
